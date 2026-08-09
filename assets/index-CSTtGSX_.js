77ed203f2b58d6fe3ec8c2ed76bc7269f450e365
Polokikiki <289390178+Polokikiki@users.noreply.github.com>
2026-08-09 01:19:27 +0000

    Deploying to gh-pages from @ Polokikiki/Milkonomy@7884918bfc0198dbec67225e531b4f1651932c6c 🚀

---
+++ assets/index-CSTtGSX_.js
@@ -0,0 +1,1 @@
+import{d as t,ay as r,az as a,g as o,f as s}from"./index-bx-p8XJr.js";const m=t({__name:"index",setup(n){const e=r();return a().replace({path:`/${e.params.path}`,query:e.query}),(c,p)=>(s(),o("div"))}});export{m as default};
