d766cf493cb37e0b20cd98c3463cc876d8598c1e
Polokikiki <289390178+Polokikiki@users.noreply.github.com>
2026-08-30 10:08:53 +0000

    Deploying to gh-pages from @ Polokikiki/Milkonomy@e709231562592809db7a6eecd3c7b312970a5340 🚀

---
+++ assets/raf-DqKudrHn.js
@@ -0,0 +1,1 @@
+import{ba as o}from"./index-CIb1NUkT.js";const e=i=>o?window.requestAnimationFrame(i):setTimeout(i,16),a=i=>o?window.cancelAnimationFrame(i):clearTimeout(i);export{a as c,e as r};
---
+++ assets/validator-C9TMqAXN.js
@@ -0,0 +1,1 @@
+import{bC as i}from"./index-CIb1NUkT.js";const n=o=>["",...i].includes(o);export{n as i};
