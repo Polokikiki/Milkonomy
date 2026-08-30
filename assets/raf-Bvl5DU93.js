74487d9d2a6d45e4ed64148c787dc0cb0762fb4e
Polokikiki <289390178+Polokikiki@users.noreply.github.com>
2026-08-30 10:52:18 +0000

    Deploying to gh-pages from @ Polokikiki/Milkonomy@4f08864c58a77b58c0fdbf19d62107b460013dcd 🚀

---
+++ assets/raf-Bvl5DU93.js
@@ -0,0 +1,1 @@
+import{ba as o}from"./index-DE4hi0px.js";const e=i=>o?window.requestAnimationFrame(i):setTimeout(i,16),a=i=>o?window.cancelAnimationFrame(i):clearTimeout(i);export{a as c,e as r};
---
+++ assets/validator-B-JVpT2q.js
@@ -0,0 +1,1 @@
+import{bC as i}from"./index-DE4hi0px.js";const n=o=>["",...i].includes(o);export{n as i};
