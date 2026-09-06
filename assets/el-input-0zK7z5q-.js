0069248ef5768187c371945b1d2cc7c832e79d16
Polokikiki <289390178+Polokikiki@users.noreply.github.com>
2026-09-06 15:44:50 +0000

    Deploying to gh-pages from @ Polokikiki/Milkonomy@322203848ac5185850d39d886f2ba09a5bad8108 🚀

---
+++ assets/el-input-0zK7z5q-.js
@@ -0,0 +1,1 @@
+import{b9 as i}from"./index-BKBrRtG4.js";const t=()=>i&&/firefox/i.test(window.navigator.userAgent);export{t as i};
---
+++ assets/validator-C6Z-qw1I.js
@@ -0,0 +1,1 @@
+import{cb as i}from"./index-BKBrRtG4.js";const n=o=>["",...i].includes(o);export{n as i};
